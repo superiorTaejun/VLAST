e05b9c5c8aaf4468c1673f952d5d226fc3ac65ae
superiorTaejun <taejun123459@gmail.com>
2022-06-02 21:08:18 +0900

    baba

--- settingChanger/settingChanger/SettingChanger.cs
+++ settingChanger/settingChanger/SettingChanger.cs
@@ -6,10 +6,12 @@ namespace settingChanger
 {
     class SettingChanger
     {
+        // 여기에 문제가 있다 싶음
         [DllImport("kernel32")]
         static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
         [DllImport("kernel32")]
         static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+        // 여기까지
         public static bool changeSetting(string path)
         {
             if (!File.Exists(path))
@@ -19,6 +21,8 @@ namespace settingChanger
             if (Path.GetFileName(path) == "BaseEditorPerProjectUserSettings.ini" || Path.GetFileName(path) == "EditorPerProjectUserSettings.ini")
             {
                 WritePrivateProfileString("/Script/UnrealEd.EditorLoadingSavingSettings", "bSCCAutoAddNewFiles", "False", path);
+                /*IniFile ini = new IniFile();
+                ini.*/ // 이 방식으로 고쳐야한다고 봄
             } else
             {
                 return false;
